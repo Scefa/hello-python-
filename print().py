801ed97212e2f81c87136ddbc765a773e7fa2be3
Scefa <sefacihan64@gmail.com>
2021-09-24 11:16:36 +0200

    'hello.py'

---
+++ print().py
@@ -0,0 +1,11 @@
+print()
+# comments Hello wereld van…                                    
+
+    pppp    y     y   ttttttt   h   h    oooo   nnnn          
+    p   p    y   y       t      h   h   o    o  n   n
+    p   p     y y        t      h   h   o    o  n   n
+    pppp       y         t      hhhhh   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h    oooo   n   n
+    Sefa 99067293
