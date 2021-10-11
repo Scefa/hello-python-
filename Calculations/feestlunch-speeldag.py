ba6bbd9732e4f3937b20f4d78e8351343d15edf0
Scefa <sefacihan64@gmail.com>
2021-10-11 11:49:21 +0200

    commit

---
+++ Calculations/feestlunch-speeldag.py
@@ -0,0 +1,13 @@
+Croissant = 17
+CPrijs = 0.39
+Stokbroden = 2
+SPrijs = 2.78
+Kortingbonnen = 1.5
+print(Croissant * CPrijs + (Stokbroden * SPrijs) - Kortingbonnen)
+
+Toegangsticket = 7.45
+Personen = 4
+VRGameSeatPrijs = 0.37
+Tijdx5Min = 9
+
+print(Toegangsticket * Personen + (VRGameSeatPrijs * Tijdx5Min))
